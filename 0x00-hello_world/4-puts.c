ce69a6ca74d6854d79a6f809724c85f227cfae64
Wilharty <durtcur618@gmail.com>
2022-09-09 07:53:36 -0700

    task 5: printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-	/**
-	 * Description: main - starting point for program execution
+	/*
+	 * main - entry point
+	 *
+	 * Description: printing a sentence using puts
 	 *
 	 * Return: Always 0 (Success)
 	 */
 int main(void)
 	{
-		puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle\n");
 		return (0);
 	}
