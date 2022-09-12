07a8ff4fbc9ff514d36f89b136b980e95afdbb2d
Wilharty <durtcur618@gmail.com>
2022-09-12 07:46:17 -0700

    task 0: if, else if, else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+/*Prog to print whether the nmber stored in the var n is pos or neg*/
+/**
+ * main - Entry point of program
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n == 0)
+		printf("%d is zero", n);
+	else
+		printf("%d is negative", n);
+
+	return (0);
+}
