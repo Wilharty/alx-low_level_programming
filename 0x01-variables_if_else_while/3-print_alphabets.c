e8e96b4cdf83aa9cb6f4c11fb94df04ba858a08f
Wilharty <durtcur618@gmail.com>
2022-09-13 09:22:49 -0700

    done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,20 +2,24 @@
 /**
  * main - prog that print the alph in lower, and then in upper, f by a nl
  * Return: 0 if success
-*/
+ */
 int main(void)
 {
 	char low = 'a';
+	char upp = 'A';
 
 	while
-		(low <= 'z');
+		(low <= 'z')
 	{
-		if (low != 'e' && low != 'q')
-		{
-			putchar(low);
-			low++;
-		}
-			putchar('\n');
+		putchar(low);
+		low++;
 	}
+	while
+		(upp <= 'Z')
+	{
+		putchar(upp);
+		upp++;
+	}
+		putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,24 +2,20 @@
 /**
  * main - prog that print the alph in lower, and then in upper, f by a nl
  * Return: 0 if success
- */
+*/
 int main(void)
 {
 	char low = 'a';
-	char upp = 'A';
 
 	while
-		(low <= 'z')
+		(low <= 'z');
 	{
-		putchar(low);
-		low++;
+		if (low != 'e' && low != 'q')
+		{
+			putchar(low);
+			low++;
+		}
+			putchar('\n');
 	}
-	while
-		(upp <= 'Z')
-	{
-		putchar(upp);
-		upp++;
-	}
-		putchar('\n');
 	return (0);
 }
