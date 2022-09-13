7e84d111204294d555e7f3f98ed63c05e3a522de
Wilharty <durtcur618@gmail.com>
2022-09-12 17:08:19 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio>
+#include <stdlib>
+/**
+ * main - prog that print the alph in lwcase, f by a new l
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alph_low
+
+	for (alph_low = 'a'; alph_low <= 'z'; alph_low ++)
+	putchar(alph_low);
+	printf("\n");
+
+	return (0);
+}
