cfbf3e8c6a517a989237077867d5f3f932bd6714
wilharty <richwrghtt@gmail.com>
2022-09-15 22:04:23 +0000

    task 8: done fixed7

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,11 +15,11 @@ void jack_bauer(void)
 	{
 		h++;
 		m++;
-	}
 		_putchar((h / 10) + '0');
 		_putchar((h % 10) + '0');
 		_putchar(':');
 		_putchar((m / 10) + '0');
 		_putchar((m % 10) + '0');
 		_putchar('\n');
+	}
 }
