45564e7cc2cc04838e2e3434d9b4cf49ec6fc3cf
wilharty <richwrghtt@gmail.com>
2022-09-15 20:29:37 +0000

    task 7: done fixed10

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,8 @@ int print_last_digit(int n)
 {
 	int l = (n % 10);
 
-	if (l != 0)
-	{
-		return (l);
-	}
-	else
-	{
-		return (0);
-	}
-		_putchar(l);
+	if (n < 0)
+		n = -n
+	_putchar(l + '0');
+	return (l);
 }
