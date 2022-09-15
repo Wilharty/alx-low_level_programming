ed352d96283a78b9a81ff8c5bd8123c444e66e2f
wilharty <richwrghtt@gmail.com>
2022-09-15 05:06:14 +0000

    task 2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - funct that prints the alph 10 X in low f by a nl
+ * Return: 0 if success
+ */
+void print_alphabet_x10(void)
+{
+	char low_10 = 'a';
+	int nmber;
+
+	while
+		(nmber < 11)
+	{
+		while
+			(low_10 <= 'z');
+		{
+			_putchar(low_10);
+			low_10++;
+		}
+		_putchar('\n');
+	}
+}
