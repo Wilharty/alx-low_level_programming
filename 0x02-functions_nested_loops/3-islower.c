861b1b6c57560208826ad07a32dbefc8819b2015
wilharty <richwrghtt@gmail.com>
2022-09-15 06:27:13 +0000

    task 3 done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _islower - funct that checks for low chars
+ * Return: 1 if it's low, 0 otherwise
+ */
+int _islower(int c);
+{
+	char low = 'a';
+	int c = 97;
+
+	while
+		(low <= 'z')
+	{
+		if (c <= 123)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		_putchar(low);
+		_putchar('\n');
+	}
+return (0);
+}
