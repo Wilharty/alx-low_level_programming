5111e2b62d525a225ef187b39d1700dc22fdec2c
wilharty <richwrghtt@gmail.com>
2022-09-16 12:52:07 +0000

    task 10: done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - funct that adds 2 int and return the result
+ * @a: first integer
+ * @b: second interger
+ * Return: sum if success
+ */
+int add(int a, int b)
+{
+	_putchar(a);
+	_putchar(b);
+
+	sum = a + b;
+	return (sum);
+}
