3b9c1d1e6a0236d524a9e53d37533987954befdb
wilharty <richwrghtt@gmail.com>
2022-09-16 08:42:11 +0000

    task 8: FIXED totally

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,32 +10,20 @@ void jack_bauer(void)
 	int h;
 	int m;
 
-	for (h = 0; h < 24; h++)
+	h = 0;
+	while (h < 24)
 	{
-		for (m = 0; m < 60; m++)
+		m = 0;
+		while (m < 60)
 		{
-			if (h < 10)
-			{
-				_putchar('0');
-				_putchar(h + '0');
-			}
-			else
-			{
-				_putchar((h / 10) + '0');
-				_putchar((h % 10) + '0');
-			}
-		_putchar(':');
-			if (m < 10)
-			{
-				_putchar('0');
-				_putchar(m + '0');
-			}
-			else
-			{
-				_putchar((m / 10) + '0');
-				_putchar((m % 10) + '0');
-			}
-				_putchar('\n');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar('\n');
+			m++;
 		}
+		h++;
 	}
 }
