bd3bbecfc37eca280ec5d8e9260d7ade8739fd53
wilharty <richwrghtt@gmail.com>
2022-09-16 23:20:07 +0000

    task 1: FIXED

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,19 +7,19 @@
 
 int main(void)
 {
-        int i;
-	
-	//the part in down
-        printf("Infinite loop incoming :(\n");
+	int i;
 
-        i = 0;
+	/*the part in down*/
+	printf("Infinite loop incoming :(\n");
 
-        while (i < 10)
-        {
-                putchar(i);
-        }
+	i = 0;
 
-        printf("Infinite loop avoided! \\o/\n");
+	while (i < 10)
+	{
+		putchar(i);
+	}
 
-        return (0);
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
