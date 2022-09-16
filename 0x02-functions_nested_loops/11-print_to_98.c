0d3f79811f1e0b8f90c78ccde27836138f38643c
wilharty <richwrghtt@gmail.com>
2022-09-16 13:14:53 +0000

    task 11: done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "maim.h"
+#include <stdio.h>
+/**
+ * print_to_98 - funct that print all Q nmbers from n to 98 in order, f by a nl
+ * @n: the first printed nmber
+ */
+void print_to_98(int n)
+{
+	while
+		(n < 99)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		n++;
+	}
+}
