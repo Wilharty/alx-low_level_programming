21148cd1f939ed23edfc0ad19e2588562be967c0
wilharty <richwrghtt@gmail.com>
2022-09-18 16:40:11 +0000

    task 2: done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - funct that mult txo int
+ * @a: first int variable
+ * @b: second int variable
+ * Return: 0 is success
+ */
+int mul(int a, int b);
+{
+	return ('a' * 'b');
+}
