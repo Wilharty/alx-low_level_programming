ec90f15f9c253e66ab51131217333b536f614cc2
wilharty <richwrghtt@gmail.com>
2022-09-18 17:02:05 +0000

    task 3: done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - funct that prints nmbers, 0 to 9, f by a nl
+ * Return: 0 if success
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for
+		(i = 0, i <= 9, i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}
