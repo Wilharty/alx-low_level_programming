cd4729d39542600f65c2b8dc9d1eeee3d12e698d
wilharty <richwrghtt@gmail.com>
2022-09-18 17:28:59 +0000

    task 4: done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - funct that prints nmbers: 0 to 9, not 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0', i <= '9', i++)
+	{
+		for (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+	}
