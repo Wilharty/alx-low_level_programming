bd98e64d20bf7bfed368ce3ff8f8c03aa2cf006c
wilharty <richwrghtt@gmail.com>
2022-09-20 02:58:26 +0000

    i

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,4 +5,21 @@
  */
 void rev_string(char *s)
 {
+	int i, j, k;
 
+	i = 0;
+	j = 0;
+	while
+		(s[j] != '\0')
+	{
+		j++;
+	}
+		j--;
+	while
+		(j > i)
+	{
+		k = s[j];
+		s[j--] = s[i];
+		s[i++] = k;
+	}
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - func that prints every other char of a string
+ * @str: variable to print
+ */
+void puts2(char *str);
+{
+	int i = 0;
+
+	while
+		(str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+			_putchar('\n');
+}
