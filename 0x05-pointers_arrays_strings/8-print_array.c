1e49c6a9aa0d3790be7c9b6dd971a5252aaee2c7
wilharty <richwrghtt@gmail.com>
2022-09-22 13:47:12 +0000

    kd

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 /**
  * print_array - funct that prints n elem of an arr of int, f by a nl
- * @a:
- * @n:
+ * @a: desc
+ * @n: desc
  */
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	if (n > 0)
 	{
 		for (i = 0; n > i; i++)
 		{
-			if (i != n-1)
+			if (i != n - 1)
 			{
 				printf("%d, ", a[i]);
 			}
