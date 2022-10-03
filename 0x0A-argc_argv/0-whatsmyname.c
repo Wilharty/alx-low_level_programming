fe90d0e756880a07338d7e9e3854a5ec10421700
wilharty <richwrghtt@gmail.com>
2022-10-03 09:25:00 +0000

    0x0A-begin

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name, f by a nl
+ * @argc: nmber of arguments
+ * @argv: array of argc's size
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
