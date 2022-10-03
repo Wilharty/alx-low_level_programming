b2d062a82021fdfce5f51dcc8ed273c3b70e9743
wilharty <richwrghtt@gmail.com>
2022-10-03 20:06:00 +0000

    fixed10

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,13 +7,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul = atoi(argv[1]) * atoi(argv[2]);
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 	}
 	else
-		printf("%d\n", mul;
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 }
