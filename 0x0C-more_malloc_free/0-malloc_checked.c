38a23b0df6590e0963e0f44bf05fc4a4046476d6
wilharty <richwrghtt@gmail.com>
2022-10-07 02:15:02 +0000

    0x0C- task 0, 1, 2, 3

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - allocates memory using malloc and exit if failed
+ * @b: integer
+ * Return: pointer to the array initialized or NULL
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == 0)
+	exit(98);
+
+return (m);
+}
