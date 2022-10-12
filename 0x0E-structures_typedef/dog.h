b2b69a0c2501903c3770d1f400c6220dc2f1704a
wilharty <richwrghtt@gmail.com>
2022-10-12 02:29:44 +0000

    0x0E task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defining a new type struct dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif
