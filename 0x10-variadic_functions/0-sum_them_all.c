36345d853235cd221d966c237fb18051b656bf8b
wilharty <richwrghtt@gmail.com>
2022-10-15 17:09:49 +0000

    0x10 all tasks

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all -  sum all arguments
+ * @n: int, number of undefined arguments
+ * Return: sum of args, otherwise 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i, sum = 0;
+
+	va_start(list, n);
+	if (n != 0)
+        	for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
+        	;
+	va_end(list);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_int - prints int
+ * @list: arguments from print_all
+ */
+void print_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_float - prints float
+ * @list: arguments from print_all
+ */
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * print_char - prints int
+ * @list: arguments from print_all
+ */
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * print_str - prints string
+ * @list: arguments from print_all
+ */
+void print_str(va_list list)
+{
+	char *s = va_arg(list, char *);
+
+	s == NULL ? printf("(nil)") : printf("%s", s);
+}
