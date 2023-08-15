ef136a361812ede1d2e85b20b782c2c125164759
wilharty <richwrghtt@gmail.com>
2023-08-15 18:08:53 +0000

    0x00

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prog that prints exactly a stce f by a nl using puts
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
